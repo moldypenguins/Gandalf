112a9aeed23a605c131960c55b44a84d155dc5b2
Craig Roberts <craig.roberts@crdevel.com>
2020-11-26 03:07:41 -0500

    Update admin.js

--- spells/admin.js
+++ spells/admin.js
@@ -24,11 +24,11 @@ const entities = new Entities();
 
 //const util = require('util');
 
-var Admin_leavechat_usage = entities.encode('!leavechat [chat id]');
-var Admin_leavechat_desc = 'Leaves a group, supergroup, or channel.';
-var Admin_leavechat = (args, ctx) => {
+let Admin_leavechat_usage = entities.encode('!leavechat [chat id]');
+let Admin_leavechat_desc = 'Leaves a group, supergroup, or channel.';
+let Admin_leavechat = (args, ctx) => {
   return new Promise(async (resolve, reject) => {
-    var chatid;
+    let chatid;
     if (args == null || args.length == 0) {
       chatid = ctx.chat.id;
     } else if (args.length > 0) {
