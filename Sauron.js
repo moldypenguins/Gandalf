13faa81fc12249390f77b782f659ece7159e7abb
Craig Roberts <craig.roberts@crdevel.com>
2020-11-26 03:04:26 -0500

    Update Sauron.js

--- Sauron.js
+++ Sauron.js
@@ -69,23 +69,23 @@ const util = require('util');
 let loginRequired = async (req, res, next) => {
   //console.log('LOCALS: ' + util.inspect(res.locals, false, null, true));
   //console.log('SESSION: ' + util.inspect(req.session, false, null, true));
-  if(typeof(res.locals.member) == 'undefined') {
+  if (typeof (res.locals.member) == 'undefined') {
     //req var to forward
     //console.log('REQ URL: ' + req.originalUrl);
-    if(req.originalUrl !== '/') {
-        req.session.req_url = url.format({
-            protocol: req.protocol, host: req.get('host'), pathname: req.originalUrl
-        });
+    if (req.originalUrl !== '/') {
+      req.session.req_url = url.format({
+        protocol: req.protocol, host: req.get('host'), pathname: req.originalUrl
+      });
     }
-    return res.status(401).render('unauthorized', { site_title: config.alliance.name, page_title: config.alliance.name });
+    return res.status(401).render('unauthorized', {site_title: config.alliance.name, page_title: config.alliance.name});
   } else {
-    let updated = await Member.updateOne({id:res.locals.member.id}, {last_access:Date.now()});
-    if(req.session.req_url !== undefined) {
-        let req_url = req.session.req_url;
-        req.session.req_url = undefined;
-        res.redirect(req_url);
+    let updated = await Member.updateOne({id: res.locals.member.id}, {last_access: Date.now()});
+    if (req.session.req_url !== undefined) {
+      let req_url = req.session.req_url;
+      req.session.req_url = undefined;
+      res.redirect(req_url);
     } else {
-        next();
+      next();
     }
   }
 }
@@ -97,40 +97,43 @@ db.connection.once("open", () => {
   app.set('view engine', 'ejs');
   app.use(logger('dev'));
   app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.urlencoded({extended: false}));
   app.use(express.static(path.join(__dirname, 'public')));
   app.use(session({
-    secret: config.web.session,
-    resave: true,
-    saveUninitialized: false,
-    cookie: { sameSite: 'none', secure: true }
+    secret: config.web.session, resave: true, saveUninitialized: false, cookie: {sameSite: 'none', secure: true}
   }));
   //redirect to https
   app.use((req, res, next) => {
-    if(req.headers.host.slice(0, 4) === 'www.') {
+    if (req.headers.host.slice(0, 4) === 'www.') {
       res.redirect(301, "https://" + req.headers.host.slice(4) + req.url);
     } else {
-      if (req.secure) { next(); } else { res.redirect('https://' + req.headers.host + req.url); }
+      if (req.secure) {
+        next();
+      } else {
+        res.redirect('https://' + req.headers.host + req.url);
+      }
     }
   });
   //add session objects to locals
-  app.use(async(req, res, next) => {
+  app.use(async (req, res, next) => {
     res.locals.site_theme = config.web.default_theme;
     res.locals.site_url = config.web.uri;
     res.locals.alliance_name = config.alliance.name;
     res.locals.bot_name = config.bot.username;
     res.locals.default_profile_pic = config.web.default_profile_pic;
-    
+
     res.locals.tick = await Tick.findOne().sort({id: -1});
-    
+
     res.locals.member = req.session.member;
     res.locals.applicant = req.session.applicant;
     res.locals.visitor = req.session.visitor;
-    
+
     console.log('MEMBER: ' + util.inspect(res.session, false, null, true));
-    
-    if(typeof(req.session) !== 'undefined' && typeof(req.session.member) !== 'undefined' && req.session.member != null) {
-      if(req.session.member.site_theme) { res.locals.site_theme = req.session.member.site_theme; }
+
+    if (typeof (req.session) !== 'undefined' && typeof (req.session.member) !== 'undefined' && req.session.member != null) {
+      if (req.session.member.site_theme) {
+        res.locals.site_theme = req.session.member.site_theme;
+      }
       res.locals.member.isADM = req.session.member.access === 5;
       res.locals.member.isHC = req.session.member.access >= 3 && (req.session.member.roles & 16) !== 0;
       res.locals.member.isDC = req.session.member.access >= 3 && (req.session.member.roles & 8) !== 0;
@@ -138,26 +141,28 @@ db.connection.once("open", () => {
       res.locals.member.isCMDR = req.session.member.access >= 3;
       res.locals.member.isSCNR = req.session.member.access >= 1 && (req.session.member.roles & 2) !== 0;
       res.locals.member.isMEM = req.session.member.access >= 1;
-      res.locals.member.planet = await Planet.findOne({id:res.locals.member.planet_id});
+      res.locals.member.planet = await Planet.findOne({id: res.locals.member.planet_id});
       res.locals.member.scans = {};
-      res.locals.member.scans.d = await Scan.findOne({planet_id:res.locals.member.planet_id, scantype:3}).sort({tick:-1, _id:-1});
-      if(typeof(res.locals.member.scans.d) != 'undefined' && res.locals.member.scans.d != null) { res.locals.member.scans.d.scan = await DevelopmentScan.findOne({scan_id:res.locals.member.scans.d.id}); }
-      res.locals.member.scans.a = await Scan.findOne({planet_id:res.locals.member.planet_id, scantype:8}).sort({tick:-1, _id:-1});
-      if(typeof(res.locals.member.scans.a) != 'undefined' && res.locals.member.scans.a != null) {
-        res.locals.member.scans.a.scan = await UnitScan.find({scan_id:res.locals.member.scans.a.id}); 
-        for(let j = 0; j < res.locals.member.scans.a.scan.length; j++) {
-          res.locals.member.scans.a.scan[j].ship = await Ship.findOne({id:res.locals.member.scans.a.scan[j].ship_id});
+      res.locals.member.scans.d = await Scan.findOne({planet_id: res.locals.member.planet_id, scantype: 3}).sort({tick: -1, _id: -1});
+      if (typeof (res.locals.member.scans.d) != 'undefined' && res.locals.member.scans.d != null) {
+        res.locals.member.scans.d.scan = await DevelopmentScan.findOne({scan_id: res.locals.member.scans.d.id});
+      }
+      res.locals.member.scans.a = await Scan.findOne({planet_id: res.locals.member.planet_id, scantype: 8}).sort({tick: -1, _id: -1});
+      if (typeof (res.locals.member.scans.a) != 'undefined' && res.locals.member.scans.a != null) {
+        res.locals.member.scans.a.scan = await UnitScan.find({scan_id: res.locals.member.scans.a.id});
+        for (let j = 0; j < res.locals.member.scans.a.scan.length; j++) {
+          res.locals.member.scans.a.scan[j].ship = await Ship.findOne({id: res.locals.member.scans.a.scan[j].ship_id});
         }
       }
     }
-    
+
     next();
   });
   //non-csrf routes
   app.use('/uptime', uptimeRoute);
   app.use('/parse', cors(corsOptions), parseRoute);
   app.use('/reject', cors(corsOptions), parseRoute);
-  
+
   //app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cookieParser());
   //app.use(csrf({ cookie: true }));
@@ -177,14 +182,15 @@ db.connection.once("open", () => {
     next(createError(404));
   });
   app.use((err, req, res, next) => {
-    if(err.code === 'EBADCSRFTOKEN') {
+    //csrf
+    if (err.code === 'EBADCSRFTOKEN') {
       res.status(403);
       res.send('form tampered with');
     } else {
       res.locals.message = err.message;
       res.locals.error = req.app.get('env') === 'development' ? err : {};
       res.status(err.status || 500);
-      res.render('error', { site_title: config.alliance.name, page_title: 'Error' });
+      res.render('error', {site_title: config.alliance.name, page_title: 'Error'});
     }
   });
   //start listening
