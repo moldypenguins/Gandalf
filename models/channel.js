8fdcbb67c6278f6ffa34a61cf20ac40aa03e121b
Craig Roberts <craig.roberts@crdevel.com>
2020-11-27 23:53:52 -0500

    Create channel.js

---
+++ models/channel.js
@@ -0,0 +1,27 @@
+/**
+ * Gandalf
+ * Copyright (C) 2020 Craig Roberts, Braden Edmunds, Alex High
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.html
+ **/
+const Mordor = require('../db.js');
+
+let channelSchema = Mordor.Schema({
+  id: {type:Number, unique:true, required:true},
+  title: {type:String},
+  type: {type:String}
+});
+
+module.exports = Mordor.model('Channel', channelSchema, 'Channels');
+
