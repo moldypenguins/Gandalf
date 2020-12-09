c046441478d6e4b639ddf8665db0d611ecba5293
Craig Roberts <craig.roberts@crdevel.com>
2020-12-08 19:46:41 -0500

    Update intel.js

--- models/intel.js
+++ models/intel.js
@@ -1,6 +1,23 @@
-var mongoose = require('../db.js');
+/**
+ * Gandalf
+ * Copyright (C) 2020 Craig Roberts, Braden Edmunds, Alex High
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.html
+ **/
+const Mordor = require('../db.js');
 
-var intelSchema = mongoose.Schema({
+let intelSchema = Mordor.Schema({
   planet_id: Number,
   alliance_id: Number,
   nick: String,
@@ -16,5 +33,5 @@ var intelSchema = mongoose.Schema({
   comment: String
 });
 
-module.exports = mongoose.model('Intel', intelSchema, 'Intels');
+module.exports = Mordor.model('Intel', intelSchema, 'Intels');
 
