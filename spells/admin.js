b75e789600da877886affdc5265639a7374fc836
Craig Roberts <craig.roberts@crdevel.com>
2020-12-16 22:03:46 -0500

    Update admin.js

--- spells/admin.js
+++ spells/admin.js
@@ -57,7 +57,7 @@ let Admin_listchats = (args) => {
     for(let i = 0; i<chats.length; i++) {
       console.log(chats[i].name);
     }
-    resolve('Chats: ' + chats.map(function(chat) { return `id: ${chat.id}, type: ${chat.type}, title: ${chat.title}`; }));
+    resolve(chats.map(function(chat) { return `id: ${chat.id}, type: ${chat.type}, title: ${chat.title}`; }).join('\n'));
   });
 }
 
@@ -96,7 +96,7 @@ let Admin_addgalmate = (args, ctx) => {
       //console.log('MENTIONS: ' + util.inspect(mentions, false, null, true));
       if(mentions.length <= 0) { reject(Admin_addgalmate_usage); }
 
-      
+
 
       if(!await GalMate.exists({id:mentions[0].id})) {
         let galm8 = new GalMate({id:mentions[0].id,first_name:mentions[0].first_name,last_name:mentions[0].last_name,username:mentions[0].username});
