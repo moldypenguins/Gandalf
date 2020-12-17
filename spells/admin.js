c6ae065a60a63be32e2009329fdf19c18408e1af
Craig Roberts <craig.roberts@crdevel.com>
2020-12-17 00:10:58 -0500

    Update admin.js

--- spells/admin.js
+++ spells/admin.js
@@ -96,7 +96,7 @@ let Admin_addgalmate = (args, ctx) => {
 
       let mentions = await ctx.mentions.get(ctx.message);
       //console.log('MENTIONS: ' + util.inspect(mentions, false, null, true));
-      if(mentions.length <= 0) { reject('No users found.'); }
+      if(mentions.length <= 0) { reject(`User ${tguser} not found.`); }
 
       if(!await GalMate.exists({id:mentions[0].id})) {
         let galm8 = new GalMate({id:mentions[0].id,first_name:mentions[0].first_name,last_name:mentions[0].last_name,username:mentions[0].username});
