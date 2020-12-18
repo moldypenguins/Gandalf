a40bfa87981723052f7105c66e74d67bd58384a9
Craig Roberts <craig.roberts@crdevel.com>
2020-12-17 19:18:23 -0500

    Update Sauron.js

--- Sauron.js
+++ Sauron.js
@@ -190,7 +190,7 @@ db.connection.once("open", () => {
       res.locals.message = err.message;
       res.locals.error = req.app.get('env') === 'development' ? err : {};
       res.status(err.status || 500);
-      res.render('error', {site_title: config.alliance.name, page_title: 'Error'});
+      res.render('error', {alliance_name: config.alliance.name, site_title: config.alliance.name, page_title: 'Error'});
     }
   });
   //start listening
