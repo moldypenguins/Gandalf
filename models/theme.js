821437ec4d9c3029ac16f5c1b7186e4c24e5f760
Craig Roberts <craig.roberts@crdevel.com>
2020-12-28 11:17:20 -0500

    Create theme.js

---
+++ models/theme.js
@@ -0,0 +1,27 @@
+/**
+ * Gandalf
+ * Copyright (C) 2020 Craig Roberts, Braden Edmunds, Alex High
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.html
+ **/
+const Mordor = require('../db.js');
+
+let themeSchema = Mordor.Schema({
+  name: {type:Number, unique:true, required:true},
+  navbar: {type:String, required:true, default:'light'},
+  path: {type:String, required:true, default:'public/styles/theme-name.css'},
+  active: {type:Boolean, default:true}
+});
+
+module.exports = Mordor.model('Theme', themeSchema, 'Themes');
