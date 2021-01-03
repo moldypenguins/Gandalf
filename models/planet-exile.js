5e131cbf9e1055e2f430bff8af5b1472bea82b91
Craig Roberts <craig.roberts@crdevel.com>
2021-01-03 12:55:23 -0500

    update require mordor to require Mordor

--- models/planet-exile.js
+++ models/planet-exile.js
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.html
  **/
-const Mordor = require('../mordor');
+const Mordor = require('../Mordor');
 
 let planetExileSchema = Mordor.Schema({
   hour: Number,
