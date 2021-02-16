843fda6b3cb523f5e99162f40cb2cfc5948ad348
Craig Roberts <craig.roberts@crdevel.com>
2021-02-16 06:43:29 -0500

    Update Sauron.js

--- Sauron.js
+++ Sauron.js
@@ -122,6 +122,9 @@ Mordor.connection.once("open", () => {
   //add session objects to locals
   app.use(async (req, res, next) => {
     res.locals.site_theme = config.web.themes[config.web.default_theme.toLowerCase()] ? config.web.themes[config.web.default_theme.toLowerCase()] : config.web.themes['affleck'];
+
+    console.log('THEME: ' + res.locals.site_theme)
+
     res.locals.site_url = config.web.uri;
     res.locals.alliance_name = config.alliance.name;
     res.locals.bot_name = config.bot.username;
@@ -138,7 +141,6 @@ Mordor.connection.once("open", () => {
       if(req.session.member.site_theme !== undefined && req.session.member.site_theme !== 'default' && config.web.themes[config.web.default_theme.toLowerCase()]) {
         res.locals.site_theme = config.web.themes[config.web.default_theme.toLowerCase()];
       }
-      console.log('THEME: ' + res.locals.site_theme)
       res.locals.member.isADM = req.session.member.access === 5;
       res.locals.member.isHC = req.session.member.access >= 3 && (req.session.member.roles & 16) !== 0;
       res.locals.member.isDC = req.session.member.access >= 3 && (req.session.member.roles & 8) !== 0;
