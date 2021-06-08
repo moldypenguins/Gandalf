29bce2916129ea1520ae868780d270d2c346b2f7
Craig Roberts <craig.roberts@crdevel.com>
2021-06-08 01:45:30 -0400

    Update Bilbo.js

--- Bilbo.js
+++ Bilbo.js
@@ -120,7 +120,7 @@ let load_ships = async() => {
 
 let load_ticks = async() => {
   if (!await Tick.exists({})) {
-    await new Tick({_id: Mordor.Schema.Types.ObjectId, tick:0, timestamp:Functions.isValidDate(argv.start) ? argv.start : null}).save();
+    await new Tick({tick:0, timestamp:Functions.isValidDate(argv.start) ? argv.start : null}).save();
     console.log("Added first tick!");
   } else {
     console.log("First tick already exists!");
