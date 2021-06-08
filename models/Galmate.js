70fb60842d353fb982611333ca9651ba5b7c98c3
Craig Roberts <craig.roberts@crdevel.com>
2021-06-08 01:35:41 -0400

    Update Galmate.js

--- models/Galmate.js
+++ models/Galmate.js
@@ -23,7 +23,7 @@
 
 const Mordor = require('../Mordor');
 
-let GalMateSchema = Mordor.Schema({
+let GalMateSchema = new Mordor.Schema({
   _id:                    {type:Mordor.Schema.Types.ObjectId, required:true},
   telegram_id:            {type:Number, unique:true, required:true},
   telegram_first_name:    {type:String},
