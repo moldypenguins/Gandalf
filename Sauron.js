f237872bf293a0dce21f56d247387d9cfd7bf505
Craig Roberts <craig.roberts@crdevel.com>
2021-06-19 08:46:54 -0400

    Update Sauron.js

--- Sauron.js
+++ Sauron.js
@@ -79,7 +79,7 @@ const util = require('util');
 let loginRequired = async (req, res, next) => {
   console.log('LOCALS: ' + util.inspect(res.locals, false, null, true));
   console.log('SESSION: ' + util.inspect(req.session, false, null, true));
-  if (typeof(req.session.member) == 'undefined') {
+  if(typeof(req.session.member) == 'undefined') {
     //req var to forward
     //console.log('REQ URL: ' + req.originalUrl);
     if (req.originalUrl !== '/') {
