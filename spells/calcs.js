968f0ef23a8d83c9c50e80a8c0c3eb23e9dd5376
Craig Roberts <craig.roberts@crdevel.com>
2021-08-23 05:44:34 -0400

    Update calcs.js

--- spells/calcs.js
+++ spells/calcs.js
@@ -40,14 +40,14 @@ let Calcs_exile_usage = he.encode('!exile');
 let Calcs_exile_desc = 'Shows information regarding chances of landing in desired galaxies.';
 let Calcs_exile = (args) => {
   return new Promise(async(resolve, reject) => {
-    let galaxy_count = await Galaxy.countDocuments({active: true, $where: `x != 200`});
+    let galaxy_count = await Galaxy.countDocuments({active: true, x: {$ne: 200}});
     console.log(`galaxy_count: ${galaxy_count}`);
 
     let galaxy_limit = Math.ceil(galaxy_count * 0.2);
     console.log(`galaxy_limit: ${galaxy_limit}`);
 
     let galaxies = await Galaxy.aggregate([
-      {$match: {active: true, $where: `x != 200`}},
+      {$match: {active: true}},
       {$sort: {planets: 1}},
       {$limit: galaxy_limit},
       {$group: {_id: '$planets', galaxies: {$sum: 1}}}
