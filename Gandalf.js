4f751f9e8cde10fc8818feee52b03e381a6a5e0c
Craig Roberts <craig.roberts@crdevel.com>
2022-03-26 02:49:07 -0400

    Update Gandalf.js

--- Gandalf.js
+++ Gandalf.js
@@ -138,9 +138,10 @@ Mordor.connection.once("open", () => {
     if(ctx.message && ctx.message.text && ctx.message.entities && Array.isArray(ctx.message.entities)) {
       for(let entity in ctx.message.entities) {
         if(ctx.message.entities[entity].type === 'url') {
+          /*
           let scanurl = new URL(ctx.message.text.substring(ctx.message.entities[entity].offset, ctx.message.entities[entity].length));
           console.log('SCANURL: ' + util.inspect(scanurl.searchParams, true, null, true));
-          /*
+
           if(scanurl.hostname === 'planetarion.com') {
             //let scanurl = url.parse(ctx.message.text.substr(ctx.message.entities[entity].offset, ctx.message.entities[entity].length), true);
             let page_content = await getStream(scanurl.href);
