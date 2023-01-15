b55edd6492e9900df598a79ead0c1f6cc23542db
Craig Roberts <craig.roberts@crdevel.com>
2023-01-15 03:31:59 -0500

    Update frodo.js

--- Frodo/frodo.js
+++ Frodo/frodo.js
@@ -27,7 +27,7 @@
 
 
 import Config from 'galadriel';
-import { Mordor, Tick, PlanetDump, GalaxyDump, AllianceDump } from 'Mordor';
+import { Mordor, Tick, PlanetDump, GalaxyDump, AllianceDump } from 'mordor';
 import axios from 'axios';
 import schedule from 'node-schedule';
 import minimist from 'minimist';
